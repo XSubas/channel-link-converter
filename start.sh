9a21b0241b92ee768e06b769f98aa1b8ad18bf66
XSubas <122809330+XSubas@users.noreply.github.com>
2023-01-18 12:09:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Channel-Mdisk-Converter.git /Channel-Mdisk-Converter
+  git clone https://github.com/XSubas/Channel-link-Converter.git /Channel-link-Converter
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /Channel-Mdisk-Converter
+  git clone $SOURCE_CODE /Channel-link-Converter
 fi
-cd /Channel-Mdisk-Converter
+cd /Channel-link-Converter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
