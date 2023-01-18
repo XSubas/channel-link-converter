35d0f62b5febd8521effc7ba599ee7028d814c0b
XSubas <122809330+XSubas@users.noreply.github.com>
2023-01-18 12:17:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,24 +9,24 @@ def is_enabled(value, default):
         return default
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID", "12124605"))
-API_HASH = os.environ.get("API_HASH", "5cf3577d85fd02286535ec2296934287")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5574279873:AAHb22nEOdC2dbwY_9GpU838frtUAvyFoQI")
+API_ID = int(os.environ.get("API_ID", "22952430"))
+API_HASH = os.environ.get("API_HASH", "38d1362519cd97bad6b669ecae2929cd")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5947206267:AAGlpb03qV-1zEMhTJGD3pY59FrEpT_H4-g")
 DROPLINK_API = os.environ.get("DROPLINK_API")
-MDISK_API = os.environ.get("MDISK_API", "Qu7jX9V0Sn3q1JHdxjPp")
+MDISK_API = os.environ.get("MDISK_API", "QqTaWUfW4gRZiwHu4cdg")
 ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertorBotpro")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://postbot:postbot@cluster0.ouwne8q.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "channellinkconverter")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://RoyalSubas:RoyalSubas@cluster0.7rb8ct0.mongodb.net/?retryWrites=true&w=majority")
 WEBSITE = os.environ.get('WEBSITE')
 
 #  Optionnal variables
 INCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("INCLUDE_DOMAIN").split(",")) if os.environ.get("INCLUDE_DOMAIN") else []
 EXCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("EXCLUDE_DOMAIN").split(",")) if os.environ.get("EXCLUDE_DOMAIN") else []
 CHANNELS = is_enabled((os.environ.get('CHANNELS', "True")), True)
-CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split("-1001806065563")) if os.environ.get("CHANNEL_ID") else []
+CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split("-1001849304076")) if os.environ.get("CHANNEL_ID") else []
 FORWARD_MESSAGE = is_enabled((os.environ.get('FORWARD_MESSAGE', "True")), True)
-SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://github.com/CyniteOfficial/Channel-Mdisk-Converter")
-USERNAME = os.environ.get("USERNAME", "CyniteBackup")
+SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://github.com/XSubas/Channel-link-Converter")
+USERNAME = os.environ.get("USERNAME", "Soren_Corporation")
 HEADER_TEXT = os.environ.get("HEADER_TEXT", '')
 FOOTER_TEXT = os.environ.get("FOOTER_TEXT", '')
 BANNER_IMAGE = os.environ.get("BANNER_IMAGE", '')
